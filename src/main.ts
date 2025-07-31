1ead99501d007a98f28fcc5c6bc566799c12ac5b
Yuri Stepin <enchantinggg4@gmail.com>
2025-07-31 13:47:39 +0300

    CI

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { UserService } from "./service/user.service";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(6100);
 
   // await app
   //   .get(ItemDropService)
