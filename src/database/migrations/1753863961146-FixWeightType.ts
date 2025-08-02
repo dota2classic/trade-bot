3509bc5579954e184169667f96d8169922e020e2
Yuri Stepin <enchantinggg4@gmail.com>
2025-08-03 00:58:42 +0300

    Fix

--- src/database/migrations/1753863961146-FixWeightType.ts
+++ src/database/migrations/1753863961146-FixWeightType.ts
@@ -5,7 +5,7 @@ export class FixWeightType1753863961146 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "item_drop_tier" DROP COLUMN "weight"`);
-        await queryRunner.query(`ALTER TABLE "item_drop_tier" ADD "weight" double precision NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "item_drop_tier" ADD "weight" double precision NOT NULL DEFAULT 0`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
