9d61d4ed055bf3a3e715c685e14fb7486b3e4c30
Yuri Stepin <enchantinggg4@gmail.com>
2026-02-08 13:49:05 +0300

    Try this

--- src/service/item-price.service.ts
+++ src/service/item-price.service.ts
@@ -168,7 +168,19 @@ export class ItemPriceService {
     item: CEconItem,
     appId = DOTA_APPID,
   ): Promise<CMarketItem> => {
-    return this.getMarketItemByName(item.market_hash_name, appId);
+    // return this.getMarketItemByName(item.market_hash_name, appId);
+    return new Promise((resolve, reject) => {
+      this.logger.log(`Get market item by name '${name}'`);
+      return this.steam.community.getMarketItem(
+        appId,
+        name,
+        Currency.RUB,
+        (err, res) => {
+          if (err) reject(err);
+          else resolve(res as CMarketItem);
+        },
+      );
+    });
   };
 
   public getMarketItemByName = async (
